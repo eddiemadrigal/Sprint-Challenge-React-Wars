0182272bb9106f39138333d4d9ced643d06477fa
Eddie Madrigal <edmadrigal@yahoo.com>
2020-02-21 09:33:48 -0800

    Character component and dependencies added

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Character from "./Character";
+import axios from 'axios';
 import "./StarWars.css";
 
 function StarWars() {
-    return null;
+    const [person, setPerson] = useState("1");
+
+    useEffect(() => {
+        axios
+            .get(`https://swapi.co/api/people/${person}/`)
+            .then(response => {
+                console.log(response.data);
+                setPerson(response.data);
+            })
+            .catch(err => {
+                console.log("Error: ", err)
+            })
+    }, []);
+
+    return (
+        <div>
+            <button onClick={() => setPerson("2")}>Person 2</button>
+            <button onClick={() => setPerson("3")}>Person 3</button>
+            <div>
+               <Character name={person.name} />;
+             
+            </div>
+        </div>
+    )
 }
 
 export default StarWars;
